4edcf26b678ecbb39fff5c3d67ac7b0dd7e1b1bd
Soikat Rahman <hello.dev.sr@gmail.com>
2024-01-21 00:18:34 +0600

    ORM init

--- B_django_ORM/app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.index, name='index'),
-    path("books/<slug:s>", views.book_detail, name='book-details'),
-]
--- B_django_ORM/core/urls.py
+++ B_django_ORM/core/urls.py
@@ -1,7 +1,8 @@
-"""B_django_ORM URL Configuration
+"""
+URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,10 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from .views import index
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('app.urls')),
+    path("admin/", admin.site.urls),
+    path("", index, name='index'),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
