c58bad8310d0bf172547e707148ebd1b6af9a376
Soikat Rahman <hello.dev.sr@gmail.com>
2024-01-27 23:31:50 +0600

    image admin ui configured

--- E_django_ClassViews/app/admin.py
+++ E_django_ClassViews/app/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
-
+from django.utils.html import mark_safe
 from .models import Todo, PhotoModel
+
+
+class PhotoInline(admin.TabularInline):
+    model = PhotoModel
+    extra = 1
 
 
 @admin.register(PhotoModel)
 class PhotoModelAdmin(admin.ModelAdmin):
-    list_display = ('id', 'caption', 'image', 'created', 'todo')
+    list_display = ('id', 'caption', 'image', 'get_thumbnail', 'todo', 'created', )
     list_filter = ('created',)
     list_display_links = ('id', 'caption')
+
+    def get_thumbnail(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" width="50"/>')
 
 
 @admin.register(Todo)
@@ -15,3 +23,4 @@ class TodoAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'complete')
     list_display_links = ('id', 'title')
     list_filter = ('complete',)
+    inlines = [PhotoInline]
