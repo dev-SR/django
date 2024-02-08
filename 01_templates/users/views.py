7e83d4c5c016cf94497cc05008e9264aa049da06
Soikat Rahman <hello.dev.sr@gmail.com>
2024-02-08 16:40:30 +0600

    redirect after login fixed

--- 01_templates/users/views.py
+++ 01_templates/users/views.py
@@ -4,13 +4,19 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.views.generic.edit import FormView
+from django.contrib.auth import REDIRECT_FIELD_NAME
 
 
 class CustomLoginView(LoginView):
     redirect_authenticated_user = True
 
     def get_success_url(self) -> str:
-        return reverse_lazy("index")
+        redirect_to = self.request.POST.get(REDIRECT_FIELD_NAME,
+                                            self.request.GET.get(REDIRECT_FIELD_NAME, ''))
+        print(redirect_to)
+        if not redirect_to or '//' in redirect_to or ' ' in redirect_to:
+            redirect_to = reverse_lazy("index")
+        return redirect_to
 
 
 class CustomRegisterView(FormView):
