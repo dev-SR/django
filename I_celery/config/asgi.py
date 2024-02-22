8f4f85c0af157b2b6acfc26e8e976cf0fdb0ca3c
Soikat Rahman <hello.dev.sr@gmail.com>
2024-02-23 02:31:11 +0600

    celery: Django Channels + HTMX websocket

--- I_celery/config/asgi.py
+++ I_celery/config/asgi.py
@@ -1,16 +1,17 @@
-"""
-ASGI config for config project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
+from status import routing
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    )
+})
